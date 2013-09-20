7fc93f6a808d263003f9149af3b135d822fdcfd8
rajesh <rkjha.it.in@gmail.com>
2013-09-20 22:58:30 +0530

    rest api

---
+++ server/api.php
@@ -0,0 +1,101 @@
+<?php
+	require_once("Rest.inc.php");
+	
+	class API extends REST {
+	
+		public $data = "";
+		
+		const DB_SERVER = "localhost";
+		const DB_USER = "root";
+		const DB_PASSWORD = "";
+		const DB = "monthly_expense";
+		const DB_TABLE = "expense";
+		
+		private $db = NULL;
+	
+		public function __construct(){
+			parent::__construct();				// Init parent contructor
+			$this->dbConnect();					// Initiate Database connection
+		}
+		
+		/*
+		 *  Database connection 
+		*/
+		private function dbConnect(){
+			$this->db = mysql_connect(self::DB_SERVER,self::DB_USER,self::DB_PASSWORD);
+			if($this->db)
+				mysql_select_db(self::DB,$this->db);
+		}
+		
+		/*
+		 * Public method for access api.
+		 * This method dynmically call the method based on the query string
+		 *
+		 */
+		public function processApi(){
+			$func = strtolower(trim(str_replace("/","",$_REQUEST['rquest'])));
+			if((int)method_exists($this,$func) > 0)
+				$this->$func();
+			else
+				$this->response('',404);				// If the method not exist with in this class, response would be "Page not found".
+		}
+		/**
+		 * Buiding query from request or post params
+		 *
+		 * @param array $data
+		 * @param string $table
+		 */
+		private function buildQuery($data,$table) {
+			$data = array('columns'=>array(''),'where'=>array('key=val'),'orderby');
+		}
+		/**
+		 * 
+		 *
+		 */
+		private function all(){	
+			// Cross validation if the request method is GET else it will return "Not Acceptable" status
+			if($this->get_request_method() != "GET"){
+				$this->response('',406);
+			}
+			$device_id = $this->_request['device_id'];
+			$sql = mysql_query("SELECT * FROM ".self::DB_TABLE." WHERE device_id = '$device_id'", $this->db);
+			if(mysql_num_rows($sql) > 0){
+				$result = array();
+				while($rlt = mysql_fetch_array($sql,MYSQL_ASSOC)){
+					$result[] = $rlt;
+				}
+				// If success everythig is good send header as "OK" and return list of users in JSON format
+				$this->response($this->json($result), 200);
+			}
+			$this->response('',204);	// If no records "No Content" status
+		}
+		
+		private function delete(){
+			// Cross validation if the request method is DELETE else it will return "Not Acceptable" status
+			if($this->get_request_method() != "DELETE"){
+				$this->response('',406);
+			}
+			$id = (int)$this->_request['id'];
+			if($id > 0){				
+				mysql_query("DELETE FROM ".self::DB_TABLE." WHERE id = $id");
+				$success = array('status' => "Success", "msg" => "Successfully one record deleted.");
+				$this->response($this->json($success),200);
+			}else
+				$this->response('',204);	// If no records "No Content" status
+		}
+		
+		/*
+		 *	Encode array into JSON
+		*/
+		private function json($data){
+			if(is_array($data)){
+				return json_encode($data);
+			}
+		}
+	}
+	
+	// Initiiate Library
+	
+	$api = new API;
+	$api->processApi();
+?>
